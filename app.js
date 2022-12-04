e51a365da759bad20b8849176f6a7aa7c02e1b45
Rubel Hossasin <rubelrana019914@gmail.com>
2022-12-04 12:42:12 +0600

    golf-web

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express()
 const cors = require('cors')
 app.use(cors({
-      origin: "http://localhost:3000"
+      origin: "https://dancing-pavlova-b2d530.netlify.app"
 }))
 // const cookieParser = require('cookie-parser')
 const fileUpload = require("express-fileupload");
