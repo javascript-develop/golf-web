b2d7273b96b10a1ef280a92dc0070d52e3e4a04d
Rubel Hossasin <rubelrana019914@gmail.com>
2022-12-14 13:23:28 +0600

    update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express()
 const cors = require('cors')
 app.use(cors({
-      origin: "https://michigansbestgolfdeals.com"
+      origin: "https://golf-b0226.web.app"
 }))
 // const cookieParser = require('cookie-parser')
 const fileUpload = require("express-fileupload");
@@ -39,7 +39,7 @@ app.post("/pay", (req, res) => {
                   payment_method: "paypal",
             },
             redirect_urls: {
-                  return_url: "https://michigansbestgolfdeals.com/myCart/chackout/review/payment/success",
+                  return_url: "https://michigansbestgolfdeals.com/myCart/checkout/review/payment/success",
                   cancel_url: "https://michigansbestgolfdeals.com/cancel",
             },
             transactions: [
