1fac4922749b73af1dfb3a3f5a4a13e42c0804a7
Rubel Hossasin <rubelrana019914@gmail.com>
2023-01-07 21:43:29 +0600

    golf

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express()
 const cors = require('cors')
 app.use(cors({
-      origin: "https://michigansbestgolfdeals.com"
+      origin: "/"
 }))
 // const cookieParser = require('cookie-parser')
 const fileUpload = require("express-fileupload");
